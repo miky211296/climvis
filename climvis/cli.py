a92f9baba67183c0894a84724e985a7455357c7b
stefano <stefano.campostrini@studenti.unitn.it>
2019-12-02 13:29:17 +0100

    Implemented argparse without defaults and choices. No docs.

--- climvis/cli.py
+++ climvis/cli.py
@@ -1,6 +1,7 @@
 import webbrowser
 import sys
 import climvis
+import argparse
 
 #aggiungere nell HELP windrose: windrose visualization for selected station
 # and number of days
@@ -61,9 +62,58 @@ def cruvis_io(args):
               'Type "cruvis --help" for usage options.')
 
 
+def print_version():
+    print('cruvis: ' + climvis.__version__)
+    print('License: public domain')
+    print('cruvis is provided "as is", without warranty of any kind')
+    
+def cruvis_location(lon, lat, nobrowser):
+    html_path = climvis.write_html(lon, lat)
+    if nobrowser:
+        print('File successfully generated at: ' + html_path)
+    else:
+        webbrowser.get().open_new_tab(html_path)
+        
+def cruvis_windrose(station, days, nobrowser):
+    html_path_windrose = climvis.write_html_wind_rose(station, days)
+    if nobrowser:
+        print('File successfully generated at: ' + html_path_windrose)
+    else:
+        webbrowser.get().open_new_tab(html_path_windrose)
+    
 def cruvis():
     """Entry point for the cruvis application script"""
 
-    # Minimal code because we don't want to test for sys.argv
-    # (we could, but this is too complicated for now)
-    cruvis_io(sys.argv[1:])
+    parser = argparse.ArgumentParser(prog = 'cruvis',
+                                     description = 'CRU data visualization at a selected location.')
+
+    parser.add_argument('-v','--version', action='store_const', const=True,
+                        default=False, help='print the installed version')
+    parser.add_argument('-l', '--loc', nargs=2, metavar=('LON', 'LAT'), type=int,
+                        help='the location at which the climate data must be extracted')
+    parser.add_argument('-w', '--windrose', nargs=2, metavar=('STATION', 'DAYS'),
+                        help="windrose at one of the possible stations" \
+                             "and for one of the possible days." \
+                             "possible_stations : ['innsbruck', " \
+                             "ellboegen', 'obergurgl', 'sattelberg']" \
+                             "possible_days :['1', '3', '7']")
+    parser.add_argument('--no-browser', action='store_const', const=True,
+                        dest='nobrowser',
+                        default=False, help='the default behavior is to open ' \
+                                            'a browser with the newly ' \
+                                            'generated visualisation. Set to ' \
+                                            'ignore and print the path to ' \
+                                            'the html file instead')
+    
+    arguments = parser.parse_args()
+    
+    if arguments.version:
+        print_version()
+    
+    if arguments.loc is not None:
+        lon, lat = arguments.loc
+        cruvis_location(lon, lat, arguments.nobrowser)
+        
+    if arguments.windrose is not None:
+        station, days = arguments.windrose
+        cruvis_windrose(station, days, arguments.nobrowser)
