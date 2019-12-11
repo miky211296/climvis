7e0f9c926922da0eb05fe37dc583442f4627730a
michele211296 <giurato.michele@gmail.com>
2019-12-11 16:17:26 +0100

    modified setup.py

--- setup.py
+++ setup.py
@@ -55,7 +55,8 @@ req_packages = ['numpy',
                 'netCDF4',
 		'windrose',
 		'holoviews',
-		'bokeh'
+		'bokeh',
+                'tornado'
                 ]
 
 check_dependencies(req_packages)
@@ -171,10 +172,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['motionless',
-                      'windrose',
-                      'holoviews',
-                      'bokeh',
-                      'tornado'],
+                      ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
