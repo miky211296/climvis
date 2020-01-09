dc5a46054339e67500f7e28a500ac4f4d18d65d6
michele211296 <giurato.michele@gmail.com>
2020-01-09 15:58:09 +0100

    test for cli, not completed

--- climvis/tests/test_cli.py
+++ climvis/tests/test_cli.py
@@ -1,36 +1,39 @@
-##### DPERECATED SINCE USE OF ARGPARSE, will be implemented soon.
+import climvis
+import climvis.cli as cli
+from unittest.mock import patch
 
-# Testing command line interfaces is hard. But we'll try
-# At least we separated our actual program from the I/O part so that we
-# can test that
-#import climvis
-#from climvis.cli import cruvis_io
-#
-#
-#def test_help(capsys):
-#
-#    # Check that with empty arguments we return the help
-#    cruvis_io([])
-#    captured = capsys.readouterr()
-#    assert 'Usage:' in captured.out
-#    print(captured.out)
-#
-#    cruvis_io(['-h'])
-#    captured = capsys.readouterr()
-#    assert 'Usage:' in captured.out
-#
-#    cruvis_io(['--help'])
-#    captured = capsys.readouterr()
-#    assert 'Usage:' in captured.out
-#
-#
-#def test_version(capsys):
-#
-#    cruvis_io(['-v'])
-#    captured = capsys.readouterr()
-#    assert climvis.__version__ in captured.out
-#
-#
+
+def test_print_version(capsys):
+
+    cli.print_version()
+    captured = capsys.readouterr()
+    assert climvis.__version__ in captured.out
+
+
+@patch('climvis.cli.input', side_effect=['o', 'y'])
+def test_handle_interactive(capsys):
+    cli.handle_interactive(23, 45)
+    captured = capsys.readouterr()
+    assert 'Warning: a blank plot might mean you are over ' in captured.out
+        #assert cli.handle_interactive() == 'expected_output'
+
+
+def test_cruvis_location(capsys):
+    cli.cruvis_location(23, 45, nobrowser=True, interactive=False)
+    captured = capsys.readouterr()
+    assert 'File successfully generated at: ' in captured.out
+
+
+def test_windvis_windrose(capsys):
+    cli.windvis_windrose('innsbruck', '3', nobrowser=True)
+    captured = capsys.readouterr()
+    assert 'File successfully generated at: ' in captured.out
+    
+@patch('climvis.cli.input', side_effect=['-h']) 
+def test_cruvis(capsys):
+    cli.cruvis()
+    captured = capsys.readouterr()
+    assert 'CRU data visualization at' in captured.out
 #def test_print_html(capsys):
 #
 #    cruvis_io(['-l', '12.1', '47.3', '--no-browser'])
